6a2df0020012a9a4f2073414c5a451893c6f7e14
BernardoBiazzi <bernardo.biazzi@gmail.com>
2023-03-20 14:19:36 -0300

    atualizando carteirinha

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AuthService, User } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public user!: User;
+
+  constructor(public authService: AuthService,
+    private changeDetector: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this.user = this.authService.userData;
   }
 
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -23,6 +23,7 @@ export class AuthService {
     public router: Router,
     public ngZone: NgZone // NgZone service to remove outside scope warning
   ) {
+    this.userData = JSON.parse(localStorage.getItem('user')!);
     /* Saving user data in localstorage when
     logged in and setting up null when logged out */
     this.afAuth.authState.subscribe((user) => {
